3820747c9d15611583525c769ea374ee74dd7658
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2024-04-10 21:23:37 +0530

    Add new files and configurations for testing and build process

---
+++ yugmak-24/src/Component/Day2/Day2.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+
+const Day2 = () => {
+  return (
+    <div>
+      <div className="section_home_project_2 opacity-100">
+        <div className="rotation_hide_container">
+          <div className="project_2_rotate">
+            <div className="project_2_rotate_background"></div>
+            {/* <img
+              src="https://assets-global.website-files.com/5d36b9200eef9b397822a84d/633ae71fb5d7fa7d61f28482_the-bastard-son-home-cutout4.png"
+              loading="lazy"
+              width="800"
+              className="home-project-2-floaty-image w-[100vw] max-w-[83vw] transform translate-x-[-40px] scale-100 rotate-x-0 rotate-y-0 rotate-z-0"
+              alt=""
+            /> */}
+            <div className="gradient_cover"></div>
+          </div>
+        </div>
+        <div className="container_project_2-home clearfix">
+          <div className="project_2_div">
+            {/* <img
+              src="https://assets-global.website-files.com/5d36b9200eef9b397822a84d/5d36b9200eef9b0c3e22a8bc_netflix_logo.png"
+              width="90"
+              className="opacity-100 transform translate-x-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0"
+              alt=""
+            /> */}
+            <h2 className="h2_home opacity-100 transform translate-x-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0">
+              ByteBattle{" "}
+            </h2>
+            <h3 className="big_headers_smaller opacity-100 transform translate-x-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0">
+              Where coding speed, accuracy, and problem-solving collide in a
+              thrilling competition of bytes.{" "}
+              <span className="wrap not-mob"></span>
+            </h3>
+            <p className="opacity-100 transform translate-x-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0">
+              ByteBattle is an exhilarating coding competition encompassing
+              three rounds of intense challenges: KeyboardBlitz, BugBusters, and
+              CodeBlindfold. Teams compete to showcase their coding skills,
+              speed, and precision, with each round presenting unique obstacles
+              to overcome. Join us for a high-stakes event where only the
+              sharpest minds prevail.
+            </p>
+            <a
+              href="/projects/the-bastard-son-and-the-devil-himself"
+              className="text-link home opacity-100 transform translate-x-0.1224px scale-100 rotate-x-0 rotate-y-0 rotate-z-0"
+            >
+              Register Now <span className="fa"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="section_home_project_3 opacity-95">
+        <div className="rotation_hide_container">
+          <div className="project_3_rotate">
+            <div className="project_3_rotate_background"></div>
+            <img
+              src="../../Assets/images/byteblitz.jpg"
+              loading="lazy"
+              width="629.5"
+              alt=""
+              className="home-project-3-floaty-image transform translate-x-[-3.96649px] opacity-100"
+              style={{ transformStyle: "preserve-3d" }}
+            />
+            <div className="gradient_cover"></div>
+          </div>
+        </div>
+        <div className="container_project_2-home clearfix">
+          <div className="project_1_div">
+            {/* <img
+            src="https://assets-global.website-files.com/5d36b9200eef9b397822a84d/5d36b9200eef9b0c3e22a8bc_netflix_logo.png"
+            width="90"
+            alt=""
+            className="opacity-95 transform translate-y-[5.2304px]"
+          /> */}
+            <h2 className="h2_home opacity-90 transform translate-y-[10.9314px]">
+              Inkprints 2.0
+            </h2>
+            <h3 className="big_headers_smaller opacity-80 transform translate-y-[21.482px]">
+              Where characters come alive through essays and objects spark
+              fantastical tales in a battle of creativity and imagination.
+            </h3>
+            <p className="opacity-70 transform translate-y-[33.8393px]">
+              Inkprints 2.0 is a dynamic event featuring two engaging rounds of
+              creativity and storytelling. In the first round, participants
+              choose characters and craft persuasive essays to defend them based
+              on specific parameters. The second round challenges participants
+              to invent imaginative and absurd stories inspired by given
+              objects. With each round lasting 15 and 20 minutes respectively,
+              contestants showcase their creativity and narrative prowess in a
+              competitive yet vibrant atmosphere at the 11th Block venue.
+            </p>
+            <a
+              href="/projects/no-one-gets-out-alive"
+              className="text-link home opacity-65 transform translate-y-[43.8768px]"
+            >
+              Register Now<span className="fa"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="section_home_project_2 opacity-100">
+        <div className="rotation_hide_container">
+          <div className="project_2_rotate">
+            <div className="project_4_rotate_background"></div>
+            {/* <img
+              src="https://assets-global.website-files.com/5d36b9200eef9b397822a84d/633ae71fb5d7fa7d61f28482_the-bastard-son-home-cutout4.png"
+              loading="lazy"
+              width="800"
+              className="home-project-2-floaty-image w-[100vw] max-w-[83vw] transform translate-x-[-40px] scale-100 rotate-x-0 rotate-y-0 rotate-z-0"
+              alt=""
+            /> */}
+            <div className="gradient_cover"></div>
+          </div>
+        </div>
+        <div className="container_project_2-home clearfix">
+          <div className="project_2_div">
+            {/* <img
+              src="https://assets-global.website-files.com/5d36b9200eef9b397822a84d/5d36b9200eef9b0c3e22a8bc_netflix_logo.png"
+              width="90"
+              className="opacity-100 transform translate-x-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0"
+              alt=""
+            /> */}
+            <h2 className="h2_home opacity-100 transform translate-x-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0">
+              FIFA
+            </h2>
+            <h3 className="big_headers_smaller opacity-100 transform translate-x-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0">
+              Where offline battles unfold on a shared map for ultimate victory.{" "}
+            </h3>
+            <p className="opacity-100 transform translate-x-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0">
+              FIFA offers players the chance to engage in offline battles on a
+              communal map, competing for the highest scores over 90 minutes.
+              Located at the 9th Block venue, participants immerse themselves in
+              intense gameplay, aiming for victory in this exciting gaming
+              event.
+            </p>
+            <a
+              href="/projects/the-bastard-son-and-the-devil-himself"
+              className="text-link home opacity-100 transform translate-x-0.1224px scale-100 rotate-x-0 rotate-y-0 rotate-z-0"
+            >
+              Register Now <span className="fa"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Day2;
---
+++ yugmak-24/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Day2 from '../../Component/Day2/Day2'
+
+const Home = () => {
+  return (
+
+    <div>
+        <Day2 />
+    </div>
+  )
+}
+
+export default Home
