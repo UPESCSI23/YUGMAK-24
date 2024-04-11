fc1bae61cd74948952b6d93ea9d98f471ae46e33
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-04-11 13:19:53 +0530

    added footer (final)

--- yugmak-24/src/Component/Footer/footer.jsx
+++ yugmak-24/src/Component/Footer/footer.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
   return (
     <div className="section_footer">
@@ -6,8 +9,47 @@ const Footer = () => {
         <div className="footer_text">
           © 2024 UPES-CSI. All rights reserved.
         </div>
-        <a className="footer_link" href="/privacy-policy">
-          Privacy Policy
+      </div>
+      <div className="footer_right_side">
+        <a
+          className="social_fa first"
+          href="https://www.instagram.com/upescsi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+        <a
+          className="social_fa"
+          href="https://twitter.com/upescsi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          
+        </a>
+        <a
+          className="social_fa"
+          href="https://www.youtube.com/@UPESCSIOfficial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+        <a
+          className="social_fa"
+          href="https://www.linkedin.com/company/computer-society-of-india-upes/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a
+          className="social_fa"
+          href="https://m.facebook.com/upescsi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faFacebook} />
         </a>
       </div>
     </div>
